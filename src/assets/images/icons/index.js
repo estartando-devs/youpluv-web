4ef5e09ad3ba56c0f1f1a766f82d767e609385c2
Jaja <qualquercoisa333133127@gmail.com>
2020-01-21 06:30:12 -0300

    update images / icons

--- src/assets/images/icons/index.js
+++ src/assets/images/icons/index.js
@@ -1,10 +1,12 @@
 import AboutUs from "./aboutus.png";
+import ArrowSvg from "./arrow.svg";
 import Arrow from "./arrow.png";
 import Home from "./home.png";
 import Smartphone from "./smartphone.png";
 
 const Icons = {
   AboutUs,
+  ArrowSvg,
   Arrow,
   Home,
   Smartphone
--- src/assets/images/index.js
+++ src/assets/images/index.js
@@ -1,12 +1,22 @@
 import BackgroundDesktop from "./backgroundDesktop.png";
 import BackgroundHeader from "./backgroundHeader.png";
-import illustration from "./illustration.png";
+import IllustrationApp from "./illustration-app.png";
+import Illustration from "./illustration.png";
+import LogoDevs from "./logoDevs.png";
+import LogoBrazilFoundation from "./logoBrazilFoundation.png";
+import LogoDefeçaCivil from "./logoDefeçaCivil.png";
+import LogoFundação from "./logoFundação.png";
 import Icons from "./icons";
 
 const Images = {
+  LogoBrazilFoundation,
   BackgroundDesktop,
   BackgroundHeader,
-  illustration,
+  IllustrationApp,
+  LogoDefeçaCivil,
+  LogoFundação,
+  Illustration,
+  LogoDevs,
   Icons
 };
 

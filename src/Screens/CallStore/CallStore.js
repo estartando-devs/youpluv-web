df654f6235a0bee57b07879a80eacf6fedaf9a06
Jaja <qualquercoisa333133127@gmail.com>
2020-01-21 06:29:04 -0300

    CallStore page --wip

--- src/Screens/CallStore/CallStore.js
+++ src/Screens/CallStore/CallStore.js
@@ -1,11 +1,19 @@
-import React from 'react';
-
-import { Container } from './styles';
-
+import React from "react";
+// import { LinearTextGradient } from "react-native-text-gradient";
+import { Container } from "./styles";
+import IllustrationApp from "../../assets/images/illustration-app.png";
 export default function CallStore() {
   return (
     <Container>
-        <h1>Chamada Para a loja</h1>
+      <h1>
+        Baixe o app e faça parte dessa <br />
+        iniciativa
+      </h1>
+      <img src={IllustrationApp} />
+      <button id="btn">
+        <p id="SubText">Android App On GOOGLE PLAY</p>
+        <h2 id="Text">GOOGLE PLAY</h2>
+      </button>
     </Container>
   );
 }
--- src/Screens/CallStore/styles.js
+++ src/Screens/CallStore/styles.js
@@ -1,11 +1,65 @@
-import styled from 'styled-components'
+import styled, { webkit } from "styled-components";
 
 const Container = styled.div`
-min-width:10vw;
-height:100vh;
-display:flex;
-align-items:center;
-border-top:1px solid black;
-`
+  height: 100vh;
+  display: flex;
+  min-width: 10vw;
+  align-items: center;
+  flex-direction: column;
+  justify-content: center;
+  border-top: 1px solid black;
+  h1 {
+    margin: 0 auto;
+    color: #2b9cc2;
+    font-size: 20px;
+    font-weight: bold;
+    line-height: 27px;
+    font-style: normal;
+    text-align: center;
+    font-family: "Montserrat";
+  }
+  img {
+    margin: 121px auto;
+    margin-bottom: -1px;
+  }
+  button {
+    width: 280px;
+    height: 50px;
+    outline: none;
+    border-left: none;
+    border-right: none;
+    border-radius: 40px;
+    margin-bottom: -33px;
+    box-sizing: border-box;
+    background-color: transparent;
+    border-top: 1.5px solid #a8ebfa;
+    border-bottom: 1.5px solid #ff5b5b;
 
-export { Container }
+    #SubText {
+      /* display: flex; */
+      font-size: 8px;
+      font-weight: bold;
+      line-height: 10px;
+      font-style: normal;
+      align-items: center;
+      font-family: Montserrat;
+      color: #50d1ef;
+    }
+    #Text {
+      /* display: flex; */
+      color: #ff3636;
+      font-size: 14px;
+      font-weight: bold;
+      line-height: 17px;
+      font-style: normal;
+      align-items: center;
+      font-family: Montserrat;
+    }
+    &:hover {
+      opacity: 1;
+      transition: opacity 500ms ease-in;
+    }
+  }
+`;
+
+export { Container };

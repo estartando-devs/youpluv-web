5aaa4b77ac3e940c8be2b0c375ab5dad71bed031
Jaja <qualquercoisa333133127@gmail.com>
2020-01-21 06:29:55 -0300

    Footer page --wip

--- src/Screens/Footer/Footer.js
+++ src/Screens/Footer/Footer.js
@@ -1,10 +1,19 @@
-import React from 'react';
-import { Container } from './styles';
+import React from "react";
+import { Container } from "./styles";
+import Images from "../../assets/images";
 
 export default function Footer() {
   return (
     <Container>
-        <h1>Footer</h1>
+      <h2>Desenvolvido por:</h2>
+      <img src={Images.LogoDevs} alt="" />
+
+      <h2>Apoio:</h2>
+      <img src={Images.LogoBrazilFoundation} alt="" />
+      <img src={Images.LogoFundação} alt="" />
+
+      <h2>Em parceria:</h2>
+      <img src={Images.LogoDefeçaCivil} alt="" />
     </Container>
   );
 }
--- src/Screens/Footer/styles.js
+++ src/Screens/Footer/styles.js
@@ -1,11 +1,24 @@
-import styled from 'styled-components'
+import styled from "styled-components";
 
 const Container = styled.div`
-min-width:10vw;
-height:50vh;
-display:flex;
-align-items:center;
-border-top:1px solid black;
-`
+  min-width: 10vw;
+  height: 75vh;
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+  border-top: 1px solid black;
+  h2 {
+    color: #727376;
+    font-size: 12px;
+    line-height: 15px;
+    margin: 15px auto;
+    font-style: normal;
+    font-weight: normal;
+    font-family: "Montserrat";
+  }
+  img {
+    margin: 20px auto;
+  }
+`;
 
-export { Container }
+export { Container };

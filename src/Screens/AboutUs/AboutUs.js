a9e4c9eb9261672a415aae910818f6d578f36acf
Jaja <qualquercoisa333133127@gmail.com>
2020-01-21 06:28:25 -0300

    AboutUs page  --wip

--- src/Screens/AboutUs/AboutUs.js
+++ src/Screens/AboutUs/AboutUs.js
@@ -1,11 +1,21 @@
-import React from 'react';
+import React from "react";
 
-import { Container } from './styles';
-
+import { Container } from "./styles";
+import ArrowSvg from '../../assets/images/icons/arrow.svg'
 export default function AboutUs() {
+  const PlayStoreUrl = `https://play.google.com/store/search?q`
   return (
     <Container>
-        <h1>Quem Somos</h1>
+      <h1>
+        Acesse notícias publicadas
+        <br />
+        pela Defesa Civil
+      </h1>
+      <div id="Test">
+        <img src={ArrowSvg}/>
+      </div>
+      <a href={PlayStoreUrl} target="_blank"><button>BAIXAR APP</button></a>
+      
     </Container>
   );
 }
--- src/Screens/AboutUs/styles.js
+++ src/Screens/AboutUs/styles.js
@@ -1,11 +1,49 @@
-import styled from 'styled-components'
+import styled from "styled-components";
 
 const Container = styled.div`
-min-width:10vw;
-height:100vh;
-display:flex;
-align-items:center;
-border-top:1px solid black;
-`
+  min-width: 10vw;
+  height: 100vh;
+  display: flex;
+  color: #2b9cc2;
+  align-items: center;
+  flex-direction: column;
+  justify-content: center;
+  border-top: 1px solid black;
+  h1 {
+    margin: 0 auto;
+    font-size: 20px;
+    font-weight: bold;
+    line-height: 27px;
+    font-style: normal;
+    text-align: center;
+    font-family: "Montserrat";
+  }
+  #Test {
+    width: 92%;
+    height: 25em;
+    display: flex;
+    margin: 10px auto;
+    justify-content: flex-end;
+  }
+  button {
+    width: 265px;
+    height: 40px;
+    border: none;
+    outline: none;
+    color: #ffffff;
+    font-size: 14px;
+    font-weight: bold;
+    line-height: 17px;
+    font-style: normal;
+    text-align: center;
+    background: #2b9cc2;
+    border-radius: 20px;
+    font-family: "Montserrat";
+    &:hover {
+      opacity: 0.6;
+      transition: 0.8s;
+    }
+  }
+`;
 
-export { Container }
+export { Container };

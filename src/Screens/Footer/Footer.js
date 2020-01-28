22ca1ab87438804d18eddbcb32e0820aface73db
Jaja <qualquercoisa333133127@gmail.com>
2020-01-28 18:24:04 -0300

    Footer --wip desktop

--- src/Screens/Footer/Footer.js
+++ src/Screens/Footer/Footer.js
@@ -1,19 +1,26 @@
 import React from "react";
 import { Container } from "./styles";
 import Images from "../../assets/images";
+import { useMedia } from "../../hooks/useMedia";
 
 export default function Footer() {
+  const { isMedium, isSmall } = useMedia();
+
   return (
-    <Container>
-      <h2>Desenvolvido por:</h2>
-      <img src={Images.LogoDevs} alt="" />
-
-      <h2>Apoio:</h2>
-      <img src={Images.LogoBrazilFoundation} alt="" />
-      <img src={Images.LogoFundação} alt="" />
-
-      <h2>Em parceria:</h2>
-      <img src={Images.LogoDefeçaCivil} alt="" />
+    <Container mobile={isSmall + isMedium}>
+      <div>
+        <h2>Desenvolvido por:</h2>
+        <img src={Images.LogoDevs} alt="" />
+      </div>
+      <div>
+        <h2>Apoio:</h2>
+        <img src={Images.LogoBrazilFoundation} alt="" />
+        <img src={Images.LogoFundação} alt="" />
+      </div>
+      <div>
+        <h2>Em parceria:</h2>
+        <img src={Images.LogoDefeçaCivil} alt="" />
+      </div>
     </Container>
   );
 }
--- src/Screens/Footer/styles.js
+++ src/Screens/Footer/styles.js
@@ -1,12 +1,16 @@
-import styled from "styled-components";
+import styled, { css } from "styled-components";
 
 const Container = styled.div`
   min-width: 10vw;
-  height: 75vh;
+  height: 25vh;
   display: flex;
   align-items: center;
-  flex-direction: column;
-  border-top: 1px solid black;
+  text-align: center;
+  justify-content: space-around;
+  border-top: 1px solid #c4c4c4;
+  div {
+    flex: 1;
+  }
   h2 {
     color: #727376;
     font-size: 12px;
@@ -17,8 +21,20 @@ const Container = styled.div`
     font-family: "Montserrat";
   }
   img {
-    margin: 20px auto;
+    margin: 20px 15px;
   }
+  ${props =>
+    props.mobile &&
+    css`
+      height: 75vh;
+      flex-direction: column;
+
+      div {
+        display: flex;
+        align-items: center;
+        flex-direction: column;
+      }
+    `};
 `;
 
 export { Container };

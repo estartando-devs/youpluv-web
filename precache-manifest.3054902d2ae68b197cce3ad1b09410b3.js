8cb646b8c2b9fb1bbfa57a612b797fb7c3bedc0f
Alan Araujo <alan.techsafe@hotmail.com>
2020-01-28 23:19:10 -0300

    Updates

---
+++ precache-manifest.3054902d2ae68b197cce3ad1b09410b3.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a4e6efc8fc39ad7c96d793d4812c874b",
+    "url": "/youpluv-web/index.html"
+  },
+  {
+    "revision": "1e14f7277554e06fe17a",
+    "url": "/youpluv-web/static/css/main.b323f77d.chunk.css"
+  },
+  {
+    "revision": "93e0874f26763f773d8c",
+    "url": "/youpluv-web/static/js/2.8a80f23f.chunk.js"
+  },
+  {
+    "revision": "fd228f9b9a68ce6b23a6cf8b0a38a0e3",
+    "url": "/youpluv-web/static/js/2.8a80f23f.chunk.js.LICENSE"
+  },
+  {
+    "revision": "1e14f7277554e06fe17a",
+    "url": "/youpluv-web/static/js/main.169cd45e.chunk.js"
+  },
+  {
+    "revision": "d7a962438769785460ae",
+    "url": "/youpluv-web/static/js/runtime-main.e349c0c3.js"
+  },
+  {
+    "revision": "ae337eddd0613ca80b830172b6762624",
+    "url": "/youpluv-web/static/media/Button.ae337edd.svg"
+  },
+  {
+    "revision": "f3515195a0618505bfa972cde8ecad7e",
+    "url": "/youpluv-web/static/media/Ellipse.f3515195.svg"
+  },
+  {
+    "revision": "79d7497d97bbde1c5e658ed7e8efc6c1",
+    "url": "/youpluv-web/static/media/arrow.79d7497d.svg"
+  },
+  {
+    "revision": "713853dcc045e66f282c6691eb647dcf",
+    "url": "/youpluv-web/static/media/backgroundDesktop.713853dc.png"
+  },
+  {
+    "revision": "e8f95d053e3768c743b378e45d82a571",
+    "url": "/youpluv-web/static/media/backgroundHeader.e8f95d05.png"
+  },
+  {
+    "revision": "5797aef7a94a8ad2314be4aeb5c95e0c",
+    "url": "/youpluv-web/static/media/illustration-app.5797aef7.png"
+  },
+  {
+    "revision": "331ed1d224ba238c6e2689c992d0016b",
+    "url": "/youpluv-web/static/media/smartphone.331ed1d2.png"
+  }
+]);

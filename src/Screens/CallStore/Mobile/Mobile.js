caef4f087b14cd50fe7b0de49989d4515600abc4
Jaja <qualquercoisa333133127@gmail.com>
2020-01-31 21:01:00 -0300

    Fix

--- src/Screens/CallStore/Mobile/Mobile.js
+++ src/Screens/CallStore/Mobile/Mobile.js
@@ -12,15 +12,17 @@ export default function AboutUs() {
     <Container desktop={isSmall + isMedium} mobile={isSmall + isMedium}>
       <h1>Baixe o app e faça parte dessa iniciativa</h1>
       <img src={IllustrationApp} />
-      <div class="containerButton">
-        <div class="circle">
-          <img src={Playstore} alt="" />
+      <a href={PlayStoreUrl} target="_blank">
+        <div class="containerButton">
+          <div class="circle">
+            <img src={Playstore} alt="" />
+          </div>
+          <button id="btn">
+            <p id="subText">Android App On GOOGLE PLAY</p>
+            <h2 id="Text">GOOGLE PLAY</h2>
+          </button>
         </div>
-        <button id="btn">
-          <p id="subText">Android App On GOOGLE PLAY</p>
-          <h2 id="Text">GOOGLE PLAY</h2>
-        </button>
-      </div>
+      </a>
     </Container>
   );
 }

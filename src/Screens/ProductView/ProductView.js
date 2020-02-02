5cc7f80673442bfd1eb22234d03cf77fa1b7fe30
Breno Nunes <Brenontavares@gmail.com>
2020-02-02 18:07:45 -0300

    changing slide text

--- src/Screens/ProductView/ProductView.js
+++ src/Screens/ProductView/ProductView.js
@@ -6,58 +6,60 @@ import Smartphone from "../../assets/images/smartphone.png";
 import Mobile from "./Mobile/Mobile";
 import { Slide } from "react-slideshow-image";
 import Images from "../../assets/images";
+
 export default function AboutUs() {
+  
+  const textsSlide = {
+    0: "O clima em tempo real na palma da sua mão",
+    1: "Construa seu pluviômetro caseiro e envie dados de chuvas quandoquiser",
+    2: "Acesse notícias publicadas diretamente pela Defesa Civil"
+  }
+
   const { isMedium, isSmall, isLarge } = useMedia();
+  const [ currentSlideText, setCurrentSlideText ] = useState(textsSlide["0"]);
   const properties = {
     duration: 8000,
     transitionDuration: 700,
     // infinite: true
     // indicators: true,
-    arrow: false
-    // onChange: (oldIndex, newIndex) => {
-    //   console.log(`slide transition from ${oldIndex} to ${newIndex}`);
-    // }
+    arrow: false,
+    onChange: (newIndex) => {
+      // console.log(`slide transition to ${newIndex}`);
+      setCurrentSlideText(textsSlide[`${newIndex}`]);
+    }
   };
-  const [texts, setTexts] = useState([
-    { id: 1, Title: "O clima em tempo real na palma da sua mão" },
-    {
-      id: 2,
-      Title:
-        "Construa seu pluviômetro caseiro e envie dados de chuvas quandoquiser"
-    },
-    { id: 3, Title: "Acesse notícias publicadas diretamente pela Defesa Civil" }
-  ]);
-  // const firstText = arrayTexts.shift()
 
   const PlayStoreUrl = `https://play.google.com/store/search?q`;
+
   return isLarge ? (
     <Container desktop={isSmall + isMedium} mobile={isSmall + isMedium}>
-      <div class="content" desktop={isSmall + isMedium}>
-        <Slide {...properties} class="slide">
+      <div className="content" desktop={isSmall + isMedium}>
+        <Slide {...properties} className="slide">
           <div className="each-slide slideImgs">
             <div>
               {/* <h1>O clima em tempo real na palma da sua mão</h1> */}
-              <img class="slideImgs" src={Images.SmartPhoneHome} alt="" />
+              <img className="slideImgs" src={Images.SmartPhoneHome} alt="" />
             </div>
           </div>
           <div className="each-slide slideImgs">
             <div>
               {/* <h1>O clima em tempo real na palma da sua mão</h1> */}
-              <img class="slideImgs" src={Images.Smartphone} alt="" />
+              <img className="slideImgs" src={Images.Smartphone} alt="" />
             </div>
           </div>
           <div className="each-slide slideImgs">
             <div>
               {/* <h1>O clima em tempo real na palma da sua mão</h1> */}
-              <img class="slideImgs" src={Images.SmartPhoneFeed} alt="" />
+              <img className="slideImgs" src={Images.SmartPhoneFeed} alt="" />
             </div>
           </div>
         </Slide>
         <div id="containerTexts">
           <h1>
-            {texts.map(text => (
+            {/* {texts.map(text => (
               <h1 key={text.id}>{text.Title}</h1>
-            ))}
+            ))} */}
+            {currentSlideText}
           </h1>
           <p>
             Basta assistir ao nosso tutorial de como construir seu pluviômetro e
